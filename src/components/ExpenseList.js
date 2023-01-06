48ac809421e86ddbe7f16b42702327bdd1b56700
Jonathan Lao <jlao@justworks.com>
2023-01-06 15:28:23 -0500

    Wrapping components

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,16 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card.js"
+import "./ExpenseList.css"
 
 function ExpenseList (props) {
+  const expenseList = props.expenses.map((expense) => (
+    <ExpenseItem
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    ></ExpenseItem>
+  ));
+
   return (
-    <div>
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        ></ExpenseItem>
-      ))}
-    </div>
+    <Card className="expenses">
+      {expenseList}
+    </Card>
   );
 }
 
